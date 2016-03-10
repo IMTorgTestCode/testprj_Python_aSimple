c662705e38736f42c99202c5daec0dba18613457
IMTorg <jason.beach@mgmt-tech.org>
2016-03-10 18:04:36 +0000

    new class Animal with many new fields

---
+++ aSimple/Animal.py
@@ -0,0 +1,34 @@
+
+
+
+#from abc import ABCMeta, abstractmethod
+
+
+
+
+
+class Animal:
+	
+	MaxAnimals = 5
+	NumOfAnimals = 0				# @userstory1
+	
+	name = "name"
+	type = "NA"
+	sound = "rooar"
+	weight = 0
+	color = "gray"
+			
+	def __init__(self, name):
+		self.name = name
+		Animal.NumOfAnimals += 1	# @userstory1
+	
+	def getName(self):
+		print self.name
+	
+	def setSound(self, s):
+		self.sound = s
+	
+	def getSound(self):
+		print self.name + " says: " + self.sound
+
+
