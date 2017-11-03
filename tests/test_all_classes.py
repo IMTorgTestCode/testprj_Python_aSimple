8e9d8b18f6a17c3b83f7230aa1bd569681c50fe2
Jason <jason.beach@mgmt-tech.org>
2017-11-03 15:27:42 -0400

    add unit test

--- tests/test_all_classes.py
+++ tests/test_all_classes.py
@@ -1,1 +1,14 @@
 import pytest
+
+
+def test_Dog(capfd):
+	from aSimple import Dog
+
+	tmpDog = Dog('name')
+	tmpDog.getSound()
+	out, err = capfd.readouterr()
+	assert out == "name says: bark"
+
+
+
+
